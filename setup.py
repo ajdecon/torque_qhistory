c97915d0c94902c0278c27d2ba7d9056dece7c6b
Adam DeConinck <ajdecon@ajdecon.org>
2012-11-25 05:18:50 +0000

    Added man page for qhistory(1).
    Original versions are written in MarkDown; man and html versions are generated
    by ronn: http://rtomayko.github.com/ronn/
    
    Hack was necessary to get distutils bdist_rpm to properly install man pages,
    due to the automatic brp-compress step used by rpmbuild. Build was made possible
    by adding the following line to ~/.rpmmacros:
    
        %__os_install_post %{nil}

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 setup(name="torque-qhistory",
-      version="0.2",
+      version="0.2.1",
       description="Simple script to help view PBS/Torque accounting files",
       author="Adam DeConinck",
       author_email="ajdecon@ajdecon.org",
       url="https://github.com/ajdecon/torque_qhistory",
       scripts=["qhistory"],
+      data_files=[('/usr/share/man/man1',['man/qhistory.1'])],
       packages=["torque_accounting"],
       license="Apache 2.0")
