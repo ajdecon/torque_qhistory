f5d9fb677448029a826a0f00681a648b97e3e9d1
Derek Gottlieb <derek.gottlieb@gmail.com>
2013-06-28 14:41:53 -0500

    Updated setup.py to reflect my contribution

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name="torque-qhistory",
-      version="0.3.4",
+      version="0.3.4.dg",
       description="Simple script to help view PBS/Torque accounting files",
-      author="Adam DeConinck",
-      author_email="ajdecon@ajdecon.org",
+      author="Adam DeConinck / Derek Gottlieb",
+      author_email="ajdecon@ajdecon.org, derek.gottlieb@gmail.com",
       url="https://github.com/ajdecon/torque_qhistory",
       scripts=["qhistory"],
       data_files=[('/usr/share/man/man1',['man/qhistory.1'])],
